4e68d212c76816d6480d9d39e3fad835a9e73610
Mikhail <mishanyzxc@gmail.com>
2020-04-20 10:33:12 +0300

    1ST COMMIT

---
+++ src/main/java/com/example/voda/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.example.voda.repos;
+
+import com.example.voda.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+   List<Message> findByTag(String tag);
+
+}
